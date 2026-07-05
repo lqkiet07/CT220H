2e8d25aafdcd06130c34a49e03fcbdcfc68bdaf7
Leooo-cpu <thuanloi2108@gmail.com>
2026-07-05 23:28:40 +0700

    feat: config Chopper API service va gen code

--- cinema_app/lib/data/remote/api_service.dart
+++ cinema_app/lib/data/remote/api_service.dart
@@ -1,17 +1,31 @@
 import 'package:chopper/chopper.dart';
 
-// TODO: (Thành viên A) Bỏ comment dòng dưới ở Giai đoạn 2
-// part 'api_service.chopper.dart'; 
 
-// @ChopperApi(baseUrl: '/api')
+part 'api_service.chopper.dart';
+
+
+@ChopperApi(baseUrl: '/api')
 abstract class ApiService extends ChopperService {
-  
-  // TODO: (Thành viên A) Định nghĩa các Endpoint API tại đây
-  // @Get(path: '/movies')
-  // Future<Response> getMovies();
+
+
+  // API 1: Lấy danh sách phim đang chiếu
+  @Get(path: '/movies')
+  Future<Response> getMovies();
+
+  // API 2: Lấy chi tiết một bộ phim cụ thể (truyền ID phim vào)
+  @Get(path: '/movies/{id}')
+  Future<Response> getMovieDetail(@Path('id') String movieId);
+
+  // API 3: Lấy sơ đồ ghế của một suất chiếu
+  @Get(path: '/showtimes/{showtimeId}/seats')
+  Future<Response> getSeats(@Path('showtimeId') String showtimeId);
+
+  // API 4: Đặt vé (Gửi dữ liệu người dùng đặt lên server)
+  @Post(path: '/tickets/book')
+  Future<Response> bookTicket(@Body() Map<String, dynamic> ticketData);
+
 
   static ApiService create([ChopperClient? client]) {
-    // return _$ApiService(client);
-    throw UnimplementedError("Hàm này sẽ được chạy khi sinh code ở Giai đoạn 2");
+        return _$ApiService(client);
   }
 }
